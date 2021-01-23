3a8b1cfdaa4c803b04e6c3845d87b3292f831c68
SajjadBrother <76851911+SajjadBrother@users.noreply.github.com>
2021-01-23 23:11:09 +0600

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,49 @@
+//https://github.com/SajjadBrother/assignment3
+
+
+
+//Kilometer To Meter convertion
+function kilometerToMeter(num) {
+    var kilometer = num * 1000;
+    return kilometer;
+}
+
+//Program for calculate my budget(BudgetCalculator)
+function BudgetCalculator(budget){
+    var watch = 100 * budget;
+    var phone = 200 * budget;
+    var laptop = 500 * budget;
+    var total = watch + phobe + laptop;
+
+    return total;
+}
+var result = BudgetCalculator(total);
+console.log(result);
+
+//Program for calculate hotel cost
+
+function hotelCost(perDay){
+    for(i=0; i <=10; i++){
+        if(i <= 10){
+            perDay = 100;
+        }
+    }
+    for (i = 11; i <= 20; i++){
+        if(i <= 20){
+            perDay = 80;
+        }
+    }
+    for(i = 21; i < n; i++){
+        if(i > 21){
+            perDay = 50;
+        }
+    }
+    return perDay;
+}
+
+//find the larger name of friend
+var friend = ['Asif', 'Ahnaf', 'Abir', 'Ayan', 'Miraz', 'Asraful']
+
+
+
+
